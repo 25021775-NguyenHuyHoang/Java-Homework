f26c0ef39a83a9a0c1ed85d9ce5779389d3818e6
Nguyễn Huy Hoàng <25021775@vnu.edu.vn>
2026-04-22 17:22:58 +0700

    Hoàn thành Bài 3 CI CD

---
+++ Bai3/src/test/java/com/banksystem/BankServiceTest.java
@@ -0,0 +1,13 @@
+package com.banksystem;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BankServiceTest {
+    @Test
+    void testWithdrawSuccess() {
+        BankService bank = new BankService();
+        // Kiểm tra xem rút 500 có bị lỗi không (giả sử số dư ban đầu 1000)
+        assertDoesNotThrow(() -> bank.withdraw(500));
+    }
+}
