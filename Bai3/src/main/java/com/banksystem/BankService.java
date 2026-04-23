70fde2132baf374643415e66f1d60fbc88caad68
Nguyễn Huy Hoàng <25021775@vnu.edu.vn>
2026-04-23 17:35:22 +0700

    Cố tình tạo lỗi code

--- Bai3/src/main/java/com/banksystem/BankService.java
+++ Bai3/src/main/java/com/banksystem/BankService.java
@@ -1,17 +1,15 @@
 package com.banksystem;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class BankService {
-    private static final Logger logger = LoggerFactory.getLogger(BankService.class);
-    private double balance_Tai_Khoan = 1000.0;
+    private double Balance;
+
     public void withdraw(double amount) {
-        if (amount > balance_Tai_Khoan) {
-            logger.error("Giao dịch thất bại: Số dư không đủ. Cần rút: {}, Hiện có: {}", amount, balance_Tai_Khoan);
-        } else {
-            balance_Tai_Khoan -= amount;
-            logger.info("Giao dịch thành công. Số dư còn lại: {}", balance_Tai_Khoan);
+        if(amount>Balance) {
+            return;
         }
+        Balance -= amount;
+    }
+    public void Nap_Tien(double amount) {
+        Balance += amount;
     }
 }
