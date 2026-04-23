41589c76f07bdb78f7f3befa4dd5c5df4d066327
Nguyễn Huy Hoàng <25021775@vnu.edu.vn>
2026-04-23 17:39:26 +0700

    Cố tình tạo code lỗi

--- Bai3/src/main/java/com/banksystem/BankService.java
+++ Bai3/src/main/java/com/banksystem/BankService.java
@@ -1,15 +1,26 @@
 package com.banksystem;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BankService {
-    private double Balance;
+    private static final Logger logger = LoggerFactory.LoggerFactory.getLogger(BankService.class);
+
+    // Loi 1: Ten bien co dau gach duoi va viet hoa (Sai quy tac Java)
+    private double balance_Tai_Khoan = 1000.0;
 
     public void withdraw(double amount) {
-        if(amount>Balance) {
-            return;
+        // Loi 2: Thieu khoang trang sau 'if' va quanh dau '>'
+        if(amount>balance_Tai_Khoan) {
+            logger.error("Giao dich that bai: So du khong du.");
+        } else {
+            balance_Tai_Khoan -= amount;
+            logger.info("Giao dich thanh cong.");
         }
-        Balance -= amount;
     }
-    public void Nap_Tien(double amount) {
-        Balance += amount;
+
+    // Loi 3: Ten phuong thuc co dau gach duoi
+    public double get_Balance() {
+        return balance_Tai_Khoan;
     }
 }
